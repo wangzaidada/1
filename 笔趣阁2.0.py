c5e5100e9b724729d4ef205cdc855444a2c90fc4
wangzaidada <abcduan@outlook.com>
2023-05-23 17:10:20 +0800

    Default Changelist

---
+++ 笔趣阁2.0.py
@@ -0,0 +1,60 @@
+# -*- coding:utf-8 -*-
+"""
+作者：DUAN
+日期：2021年11月18日
+"""
+import requests
+from bs4 import BeautifulSoup
+
+headers = {                                  # 请求头伪装
+        'User-Agent': 'Mozilla/5.0'
+    }
+
+
+def getHTMLText(url):
+    try:
+        r = requests.get(url, headers, timeout=30)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return ""
+
+
+def getList(page):
+    page_list = page.find('dl', class_="zjlist").find_all('a')  # 解析目录
+    return page_list
+
+
+def getBookInfo(lst, baseurl, book_name, book_author):
+    rp = " 笔趣阁 www.bbiquge.net，最快更新" + book_name + "最新章节！"
+    book_name = book_name + ' ' + book_author + ".txt"
+    f = open(book_name, 'w+', encoding='utf-8')
+    for l in lst:
+        new_url = baseurl + l['href']  # 定制url
+        page1 = requests.get(new_url, headers=headers).text  # 获取文章页面
+        page1_soup = BeautifulSoup(page1, 'lxml')  # 解析文章页面
+        txt = page1_soup.find('div', id='content').text  # 提取文章内容
+        txt = txt.replace(rp, '')  # 替换多余字符串
+        txt = txt.replace("     ", '\n  ')
+        f.write('\n' + l.text)
+        f.write(txt)
+        print('\r' + l.text + ' 已保存', end='')
+    f.close()
+    print('\n爬取结束')
+    return True
+
+
+def main():
+    url = "https://www.bbiquge.net/book_21110/"  # 笔趣阁网址
+    page = getHTMLText(url)
+    page = BeautifulSoup(page, 'lxml')
+    book = page.find('h1').text.split(' / ')
+    book_name, book_author = book[0], book[1]
+    book_list = getList(page)
+    print('开始下载：{}'.format(book_name))
+    getBookInfo(book_list, url, book_name, book_author)
+
+
+main()
+
